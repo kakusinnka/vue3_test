ae612b4df9e0ce016e3237239c0a4800a2fc242d
haozhenhua <1305510482@qq.com>
2025-06-21 16:03:14 +0900

    学习setup，ref，reactive

---
+++ 02_src_初识setup/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>我叫：{{ name }}</h1>
+  <h1>我今年{{ age }}岁</h1>
+  <button @click="sayHello">点击我</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  setup() {
+    let name = "张三";
+    let age = 18;
+
+    const sayHello = () => {
+      alert(`我叫${name}, 今年${age}岁`);
+    };
+
+    return {
+      name,
+      age,
+      sayHello,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- 03_ref函数/App.vue
+++ 03_ref函数/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <h1>我叫：{{ name }}</h1>
+  <h1>我今年：{{ age }}岁</h1>
   <h1>我的车是：{{ car.brand }}</h1>
   <h1>车的颜色是：{{ car.color }}</h1>
   <h1>车的价格是：{{ car.price }}</h1>
@@ -6,24 +8,30 @@
 </template>
 
 <script>
-import { reactive } from "vue";
+import { ref } from "vue";
 
 export default {
   name: "App",
   setup() {
-    let car = reactive({
+    let name = ref("张三");
+    let age = ref(18);
+    let car = ref({
       brand: "宝马",
       color: "红色",
       price: 30,
     });
 
     const changeInfo = () => {
-      car.brand = "奔驰";
-      car.color = "蓝色";
-      car.price = 50;
+      name.value = "李四";
+      age.value = 20;
+      car.value.brand = "奔驰";
+      car.value.color = "蓝色";
+      car.value.price = 50;
     };
 
     return {
+      name,
+      age,
       car,
       changeInfo,
     };
